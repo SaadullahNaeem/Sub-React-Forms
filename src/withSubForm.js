9ad32270465ac4986ca51538c1a4bc1cb8bd50b3
SaadullahNaeem <saadullah.naeem@gmail.com>
2023-02-16 17:05:26 +0500

    Initial commit

---
+++ src/withSubForm.js
@@ -0,0 +1,32 @@
+import { Formik } from "formik";
+import React from "react";
+
+const onSubmit = () => {
+  alert("saad");
+};
+
+export const withSubForm = (Component, validationSchema) => ({
+  field,
+  form,
+  fieldProps
+}) => {
+  const initialValues = field.value;
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      children={(props) => (
+        <Component
+          {...props}
+          setFieldValue={form.setFieldValue}
+          setFieldError={form.setFieldError}
+          setErrors={form.setErrors}
+          name={field.name}
+          fieldProps={fieldProps}
+        />
+      )}
+    />
+  );
+};
